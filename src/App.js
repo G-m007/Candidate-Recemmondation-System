a723e95f0720571e56d96a5aa50a1638dd946b80
Gautham H <113533687+G-m007@users.noreply.github.com>
2024-09-30 12:17:19 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar'; // Import the Navbar component
+import Home from './Home';
+import About from './About';
+import Jobs from './Jobform';
+import Contact from './Contact';
+import LoginSignup from './LoginSignup';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />  {/* Ensure Navbar is included at the top */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/About" element={<About />} />
+
+          <Route path="/candidate-login" element={<LoginSignup userType="candidate" />} />
+          <Route path="/recruiter-login" element={<LoginSignup userType="recruiter" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
